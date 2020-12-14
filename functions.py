c499f31a363d974680a151b3769f63bd7d6252d8
Bryan Quadras <bryan.quadras2001@gmail.com>
2020-12-14 14:25:12 +0100

    Updated code from Oscar's version

--- functions.py
+++ functions.py
@@ -1,101 +1,117 @@
-from math import pi, sqrt, e
+from math import e, pi, sqrt
+import numpy as np
+
 
 def get_material_properties(mat_code):
     # Dictionary of materials with their properties
     material_properties = {
         "Al7068-T6": {
-            "ult_ten_str": 710e6, #Pa,
-            "yield": 683e6, #Pa,
-            "young": 73.1e9, #Pa,
-            "bearing_strength_allow": None, #cant find,
-            "density": 2830, #kg/m3,
+            "ult_ten_str": 710e6,  # Pa,
+            "yield": 683e6,  # Pa,
+            "young": 73.1e9,  # Pa,
+            "bearing_strength_allow": None,  # cant find,
+            "density": 2830,  # kg/m3,
             "poisson": 0.32,
-            "t_required": 1.32e-3 #m
+            "t_required": 1.94e-3  # m
         },
         "Ti6Al-4V(5)": {
-            "ult_ten_str": 950e6, #Pa,
-            "yield": 920e6, #Pa,
-            "young": 113e9, #Pa,
-            "bearing_strength_allow": None, #Pa,
-            "density": 2780, #kg/m3
-            "expansion_coef": None, # m/m * K,
+            "ult_ten_str": 950e6,  # Pa,
+            "yield": 920e6,  # Pa,
+            "young": 113e9,  # Pa,
+            "bearing_strength_allow": None,  # Pa, N/a
+            "density": 2780,  # kg/m3
+            "expansion_coef": None,  # m/m * K,
             "poisson": 0.34,
-            "t_required": 0.99e-3 #m         
+            "t_required": 1.45e-3  # m
         },
         "D6AC": {
-            "ult_ten_str": 1931e6, #Pa,
-            "yield": 1724e6, #Pa,
-            "young": 210e9, #Pa,
-            "bearing_strength_allow": None, #Pa,
-            "density": 7.87e3, #kg/m3
-            "expansion_coef": None, #m/m * K,
+            "ult_ten_str": 1931e6,  # Pa,
+            "yield": 1724e6,  # Pa,
+            "young": 210e9,  # Pa,
+            "bearing_strength_allow": None,  # Pa, N/a
+            "density": 7.87e3,  # kg/m3
+            "expansion_coef": None,  # m/m * K,
             "poisson": 0.3,
-            "t_required": 0.49e-3 #m
-        }    
+            "t_required": 0.71e-3  # m
+        }
     }
     # Returns the appropriate material properties based on the given material code
     return material_properties[mat_code]
 
-def det_launch_loads(material, L, R, t_1, t_2, M_fuel, A_Z):
-    # Get properties from material
-    density = material["density"]
-    yield_stress = material["yield"]
-    E_modulus = material["young"]
-    v = material["poisson"]
-    t_1 = material["t_required"]
-    # Set t to the thickness of the walls of the fuel tanks
-    t = t_1 - 0.0001 
-    # Margin of safety
-    MS = 0.05
-    safe = False
-    counter = -1
-    p = 17.04 * 100000 #Pa
-    while not safe and counter <= 100000:
-        # Increment counter and add 0.0001 m to thickness for analysis (iteration)
-        counter += 1
-        t += 0.0001
-        # First calculate the initial mass of fuel tank mass plus fuel mass
-        M_total = (2 * pi * R * t * density) + M_fuel 
-        # Due to assumption, compressive load at the ends is the M_total time the max acceleration in z-direction
-        F_z = M_total * A_Z
-        #F_int_pressure_tension = (p - 101325) * pi * R**2  
-        # Possible if we want to consider internal pressure that applies tension
-        #F_z_total = F_z - F_int_pressure_tension
-        F_z_total = 2
-        if F_z_total < 0:
-            print(F_z_total)
-            safe = True
-            print("There will be no compression so buckling is not possible.")
-            break
-        else:
-            # Determination of cross section area
-            A = 2 * pi * (R + t)**2
-            # Determination of cross section Moment of Inertia
-            Ixx = (pi/4) * ((R + t)**4 - R**4)
-            # Determination of the critical stress for Column Buckling
-            sigma_crit_column_buckling = (pi * E_modulus * Ixx) / (A * L **2)
-            # Shell Buckling determination
-                # First use the derivative (check report) to find lambda
-            lambda_ = sqrt((12 / pi**4) * (L**4 / (R**2 * t**2)) * (1 - v**2))
-                # Secondly we find the value of k
-            k = lambda_ + (12 / pi**4) * (L**4/(R**2 * t**2)) * (1 - v**2) * (1 / (lambda_**2))
-                # Thirdly we find the value of Q
-            Q = (p / E_modulus) * (R / t)**2
-                # Finnaly we find sigma_crit_shell_buckling
-            sigma_crit_shell_buckling = (1.983 - 0.983 * e**(-23.14*Q)) * k * ((pi * E_modulus) / (12 * (1 - v **2))) * (t / L)**2
-            
-            #Comparing the stresses with material yield stress taking a 5% safety margin
-            MS_column_buckling =  (yield_stress  / sigma_crit_column_buckling) - 1
-            MS_shell_buckling = (yield_stress / sigma_crit_shell_buckling) - 1
-            
-            if MS_column_buckling > MS and MS_shell_buckling > MS:
-                safe = True
-    if safe:
-        print("safe. t =", t)
-    else:
-        print("not safe.")
-    
-def attachment_mass(F_comp, N_attach): 
-    # takes total compressive force as input, and returns mass of one attachment
-    M_attach = (F_comp / N_attach) * (0.624) / 12100
-    return M_attach
+
+def attachment_mass(F_comp, N_attach):  # takes total compressive force as input, and returns mass of one attachment
+    m_attach = (F_comp / N_attach) * (0.6) / 12100
+    return m_attach
+
+
+def SMoA_xx(R, t):  # calculates Ixx for a circle, so it's also Iyy
+    Ixx = 0.25 * pi * ((R + t) ** 4 - R ** 4)
+    return Ixx
+
+
+def column_buckling_crit(E_modulus, Ixx, r, L, t):  # calculates column buckling
+    A = pi * ((t + r) ** 2 - r ** 2)  # area of material
+    sigma_cr_column = (pi * E_modulus * Ixx) / (A * L ** 2)
+    return sigma_cr_column
+
+
+def shell_buckling_crit(E_modulus, v, t, L, R, p):
+    # Q, Lambda, k all req'd for shell buckling critical stress
+    Q = (p / E_modulus) * (R / t) ** 2  # find Q
+    lambda_ = sqrt((12 / pi ** 4) * (L ** 4 / (R ** 2 * t ** 2)) * (1 - v ** 2))  # find lambda
+    k = lambda_ + (12 / pi ** 4) * (L ** 4 / (R ** 2 * t ** 2)) * (1 - v ** 2) * (1 / (lambda_ ** 2))  # find k
+    sigma_cr_shell = (1.983 - 0.983 * e ** (-23.14 * Q)) * k * ((pi * E_modulus) / (12 * (1 - v ** 2))) * (t / L) ** 2
+    return sigma_cr_shell
+
+
+def forces_on_top(m_struct, al, az,
+                  force_SF):  # returns forces acting on the beam. SC simplified to point at centre structure
+    a_vect = np.array([al, az])
+    f_vect = a_vect * m_struct * force_SF
+    return f_vect
+
+
+def MS(sigma_Ref, sigma_Appl):
+    ms = (sigma_Ref / sigma_Appl) - 1
+    return ms
+
+
+######################################
+# working space )))))
+def calc_shear(t, Ixx, Vy, r):
+    tau = Vy * t * r / Ixx
+    return tau
+
+
+def calc_normal_axial(F_z, r, t):
+    A = pi * ((t + r) ** 2 - r ** 2)
+    sigma_axial = F_z / A
+    return sigma_axial
+
+
+def calc_normal_shear(F_lateral, length, radius, Ixx):  # Returns normal stress due to lateral shear load
+    # Find magnitude of moment due to shear
+    moment_magnitude = F_lateral * length
+
+    # Uses the bending formula (M*y/Ixx) to find normal stress due to the shear load
+    normal_due_to_shear = (moment_magnitude * radius) / Ixx
+
+    return normal_due_to_shear
+
+
+def calc_von_mises(sigma_x, sigma_y, sigma_z, tau_xy, tau_yz, tau_zx):
+    # full Von Mises equation
+    sigma = sqrt(0.5 * (sigma_x - sigma_y) ** 2 + (sigma_y - sigma_z) ** 2 + (sigma_z - sigma_x) ** 2 + 3 * (
+                tau_xy ** 2 + tau_yz ** 2 + tau_zx ** 2))
+    # sigma = sqrt((sigma ** 2) + (3 * (tau ** 2)))  # simplified, only considering one normal, and one shear stress
+    return sigma
+
+
+def hoop_stress(p, r, t): # hoop stress in pressure vessele
+    sigma_hoop = p * r / t
+    return sigma_hoop
+
+
+def axial_stress(p, r, t): # axial stress in pressure vessel
+    sigma_axial = p*r/(2*t)
+    return sigma_axial
--- main.py
+++ main.py
@@ -1,14 +1,117 @@
-from functions import det_launch_loads, get_material_properties
+import functions as fn
+from math import pi
 
-L = 1.63
-R = 1 / 2 #m
-g = 9.81 #m/s2
-Az = 4.56 * g #m/s2
-M_fuel = 1127.9 #kg
-possible_materials = ["Al7068-T6", "Ti6Al-4V(5)", "D6AC"]
+# initial dimensions
+#   probably put them in a dict
+# should include initial values, these aren't too hard to do by hand, and should be a reasonable starting point
+# initial forces, come from initial masses, should be in function so they can be called in the loop again
 
-for mat_code in possible_materials:
-    print("Trying", mat_code)
-    material = get_material_properties(mat_code)
-    det_launch_loads(material, L, R, None, None, M_fuel, Az)
-    print()
+# Basic masses (so payload, propellant and the likes, these values do not change)
+M_const = 3850 - 1802  # kg, all not-structural masses, see wp2. (yeah 1800kg's of structure is a lot innit?)
+
+# Basic dimensions (so length n shit)
+l = 4.4  # meters
+r = 0.5  # meters, minimum inner structure radius
+
+# constants
+g = 9.81  # standard gravity
+# accelerations in all directions all in m/s/s
+# Z is in negative Z direction, l is for lateral acceleration, so in XY plane.
+#   script is written such that this is ok.
+Al = 2 * g
+Az = 4.6 * g
+
+# Safety factors
+Force_SF = 1.5  # SF applied to forces
+
+MS_goal = 0.2  # well, speaks for itself innit
+
+# add loop for material fuckery
+# pull thickness from material :_)))
+p = 20e5  # Pa, tank pressure
+
+# some quick code to loop through materials
+materials = ["Al7068-T6", "Ti6Al-4V(5)", "D6AC"]
+for i in materials:
+    matprob = fn.get_material_properties(i)
+    ref_sigma_yield = matprob['yield']
+    E_modulus = matprob["young"]
+    v = matprob["poisson"]
+    t = matprob["t_required"]
+    num_iter = 0  # Counter which keeps count (no shit) on the number of iterations for the while loop
+    Tank_pass = False
+    Structure_pass = False
+    finished = False
+    while not finished and num_iter < 10000:
+        num_iter += 1  # increment counter
+        finished = True  # set finished to true, loop is broken if t is not changed
+        Ixx = fn.SMoA_xx(r, t)  # second moment of area, called Ixx, but Iyy is the same because it's a circle :)))
+        # update sc mass
+        M = l * pi * ((r + t) ** 2 - r ** 2)
+        M_total = M + M_const
+        F_vect = fn.forces_on_top(M_total, Al, Az, Force_SF)  # returns array, [F_lateral, F_compressive]
+
+        # calculate stresses
+        # calc shear (just called tau because there's only one shear stress under consideration)
+        tau_lateral = fn.calc_shear(t, Ixx, F_vect[0], r)  # shear due to lateral forces
+        # no direction is considered, as the lateral force direction can always be defined as positive x, as it rotates
+
+        # calc normal due to Az (fairly self explanatory)
+        sigma_normal_axial = fn.calc_normal_axial(F_vect [1], r, t)
+        # should be done
+
+        # calc normal due to moment caused by shear force
+        sigma_normal_shear = fn.calc_normal_shear(F_vect[0], l, r, Ixx)  # F_vect[0] is the lateral shear load
+
+        # peak normal stress, to be compared against buckling and used for Von Mises failure
+        sigma_normal_peak = sigma_normal_axial + sigma_normal_shear
+
+        # hoop stress, used in Von Mises
+        sigma_hoop = fn.hoop_stress(p, r, t)
+
+        # Von Mises stress (used because it's simpler than Tresca, possible differences solved by safety factor)
+        sigma_Von_Mises = fn.calc_von_mises(0, sigma_hoop, -sigma_normal_peak, tau_lateral, 0, 0)
+        # sigma_Von_Mises_Struct = fn.calc_von_mises(0,0,sigma_normal_peak,tau_lateral,0,0)
+
+        # calc reference stresses for buckling
+        # shell
+        ref_bkl_shell = fn.column_buckling_crit(E_modulus, Ixx, r, l, t)
+        # column
+        ref_bkl_column = fn.shell_buckling_crit(E_modulus, v, t, l, r, p)
+
+        # calculate MS shear, normal, buckling (both, duh)
+        MS_bkl_shell = fn.MS(ref_bkl_shell, sigma_normal_peak)
+        MS_bkl_column = fn.MS(ref_bkl_column, sigma_normal_peak)
+        MS_yield = fn.MS(ref_sigma_yield, sigma_Von_Mises)
+
+        # makes changes and sets loop to run again
+        if MS_goal < MS_bkl_shell < MS_goal * 1.1:
+            finished = False
+            if MS_goal > MS_bkl_shell:
+                t *= 1.01
+            else:
+                t *= 0.99
+
+        elif MS_goal < MS_bkl_column < MS_goal * 1.1:
+            finished = False
+            if MS_goal > MS_bkl_column:
+                t *= 1.01
+            else:
+                t *= 0.99
+        elif MS_goal < MS_yield < MS_goal * 1.1:
+            finished = False
+            if MS_goal > MS_yield:
+                t *= 1.01
+            else:
+                t *= 0.99
+
+        # if nothing changed, dimensions and relevant properties are printed
+        if finished:
+            print(i)
+            print('Margins of safety:')
+            print('Shell buckling: ', MS_bkl_shell)
+            print('Column buckling: ', MS_bkl_column)
+            print('Yield: ', MS_yield)
+            print('Dimensions:')
+            print('Thickness [m]: ', t)
+            print('Structure mass [kg]: ', M)
